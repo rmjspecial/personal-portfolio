b03383b313d6f018f65abe7e568828f41bd23466
rejoice <rmjspecial2015@gmail.com>
2022-06-14 10:15:54 +0100

    add app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
