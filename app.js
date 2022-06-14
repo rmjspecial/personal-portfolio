7c7185f1c118772ac5498373f92fa3fc1e0d8272
james mbonabucya <james.mbonabucya@tek-experts.com>
2022-06-14 11:24:05 +0200

    modify app.js line 9-12

--- app.js
+++ app.js
@@ -5,3 +5,9 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   mobileMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.menu-item').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
+
